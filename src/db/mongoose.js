4d90dd73d2de77e309cb7380903f55825ce43d42
vijayashreepd98 <52790991+vijayashreepd98@users.noreply.github.com>
2020-08-05 11:44:50 +0530

    Delete mongoose.js

--- src/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-let mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/eventBookingSystem', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-});
